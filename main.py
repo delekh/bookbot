999e1bda63a120a3c917dcdffedec54b38609fef
delekh <delek@live.fr>
2025-12-22 11:37:33 +0100

    Added get book text feature

---
+++ main.py
@@ -0,0 +1,12 @@
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+main()
