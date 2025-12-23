7c59940d2752b4d63dafe94bc4ce19bc397ab9d5
delekh <delek@live.fr>
2025-12-23 22:42:52 +0100

    Added book report feature.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stat.py import get_num_words
+from stats import get_num_words, get_character_count, get_ordered_character_count
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
@@ -8,7 +8,16 @@ def get_book_text(path_to_file):
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    words = word_count(text)
+    words = get_num_words(text)   
+    char_count = get_character_count(text)
+    ordered_char_count = get_ordered_character_count(char_count)
+    
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {words} total words")
-
+    print("--------- Character Count -------")
+    for char in ordered_char_count:
+        print(f"{char['char']}: {char['num']}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,25 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_character_count(text):
+    char_count = {}
+    for char in text:
+        if char.lower() in char_count:
+            char_count[char.lower()] += 1
+        else:
+            char_count[char.lower()] = 1
+    return char_count
+
+#helper function to get num of each character in a dict
+def get_num(e):
+    return e["num"]
+
+def get_ordered_character_count(character_dict):
+    sorted_character_dict_list = []
+    for char in character_dict:
+        sorted_character_dict_list += [{"char": char, "num": character_dict[char]}]
+    
+    sorted_character_dict_list.sort(key=get_num, reverse=True)
+
+    return sorted_character_dict_list
