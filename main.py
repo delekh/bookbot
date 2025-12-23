0bd586ed7f21b8ce39891d03bfafe0f1e0d63b2d
delekh <delek@live.fr>
2025-12-23 22:53:40 +0100

    Added cli book selection support.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_character_count, get_ordered_character_count
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,7 +7,10 @@ def get_book_text(path_to_file):
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     words = get_num_words(text)   
     char_count = get_character_count(text)
